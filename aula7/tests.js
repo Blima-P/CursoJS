d5c5d799b9a1bd182784cc792a3c6bf5d373d43c
Pedro Braga de Lima <p1braga.tech@gmail.com>
2026-01-06 15:25:53 -0300

    aula7

---
+++ aula7/tests.js
@@ -0,0 +1,7 @@
+let NOME = "joao";
+
+console.log(NOME ," nasceu em 1984");
+console.log("Em 2000", NOME ,"conheceu maria");
+console.log(NOME , "casou-se com maria em 2012");
+console.log("maria teve 1 filho com", NOME ,"em 2015");
+console.log("o filho de", NOME ," se chama eduardo");
