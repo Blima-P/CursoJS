0138697c8cdb7d110411083d03cace31dec9ab9f
Pedro Braga de Lima <p1braga.tech@gmail.com>
2026-01-07 09:13:32 -0300

    aula08

---
+++ index.js
@@ -0,0 +1,9 @@
+const nome = "joao";
+    const num1 = 5;
+    const num2 = 10;
+const resultado = num1 * num2;
+const resultadoduplicado = resultado * 2;
+let resultadotriplicado = resultado * 3;
+resultadotriplicado = resultadotriplicado + 5;
+console.log(typeof nome);
+
