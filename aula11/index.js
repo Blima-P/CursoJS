ec3697b31dc785c5d745727d9fe65d19f632c34c
Pedro Braga de Lima <p1braga.tech@gmail.com>
2026-01-08 13:05:37 -0300

    aula11

---
+++ aula11/index.js
@@ -0,0 +1,15 @@
+let NOME = "joao";
+const nome = "joao";
+    const num1 = 5;
+    const num2 = 10;
+const resultado = num1 * num2;
+const resultadoduplicado = resultado * 2;
+let resultadotriplicado = resultado * 3;
+resultadotriplicado = resultadotriplicado + 5;
+const peso = 84;
+const altura = 1.8;
+let imc;
+let btd;
+
+imc = peso / (altura * altura);
+btd = 2026 - idade;
